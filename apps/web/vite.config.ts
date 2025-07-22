b7c743320acda9b68f6e8b6b6e4b146fe101e81b
Piyathida San-aoun <Piyathida_s101@hotmail.com>
2025-07-22 16:28:25 +0700

    fix picture is not show

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/personal-site-monorepo/',
+  build: {
+    assetsDir: 'assets',
+    copyPublicDir: true  // เพิ่มบรรทัดนี้
+  }
 })
