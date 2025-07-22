7888d27d3c82911db73bbb7de6bd5e1a4d5063ce
Piyathida San-aoun <Piyathida_s101@hotmail.com>
2025-07-22 20:21:16 +0700

    update

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import CV from './pages/CV';
 import Admin from './pages/Admin';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/blog">Blog</Link> | <Link to="/cv">CV</Link> | <Link to="/admin">Admin</Link>
-      </nav>
+    <Router basename="/personal-site-monorepo">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/cv" element={<CV />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
